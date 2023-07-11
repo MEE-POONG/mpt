cbbe761353a46cd683c189cdb039aeea43f9fe84
devilzeros <devilzeros@outlook.co.th>
2023-07-11 16:42:22 +0700

    est

--- src/pages/news.tsx
+++ src/pages/news.tsx
@@ -1,5 +1,7 @@
 import Head from 'next/head'
 import TheLayout from '@/components/TheLayout'
+import NewsDiscover from '@/container/News/NewsDiscover'
+import NewsTopSeller from '@/container/News/NewsTopSeller'
 import NewsHead from '@/container/News/NewsHead'
 // import HomeMain from '@/container/Home/HomeMain'
 // import HomeCategories from '@/container/Home/HomeCategories'
@@ -17,6 +19,8 @@ export default function NewsPage() {
       </Head>
       <div className='news-page'>
         <NewsHead />
+        <NewsDiscover />
+        <NewsTopSeller />
         {/* <HomeMain />
         <HomeCategories />
         <HomeCreateNft />
