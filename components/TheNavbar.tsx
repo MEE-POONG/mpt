7e5458997b0790a75af922e71b7053e0c662668e
devilzeros <devilzeros@outlook.co.th>
2023-07-11 14:40:59 +0700

    nav

--- components/TheNavbar.tsx
+++ components/TheNavbar.tsx
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { useRouter } from 'next/router';
+import { LanguageContext } from '@/components/Language/LanguageContext';
 import BtnSetting from './Language/BtnSetting';
+import { navData } from '@/data/navber';
+import Link from 'next/link';
 
 const TheNavber: React.FC = () => {
+  const router = useRouter();
+  const { currentLanguage } = useContext(LanguageContext);
+
+  const navList = (
+    <ul className="mb-4 mt-2 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
+      {navData.map((navItem, index) => (
+        <li key={index}>
+          <Link href={navItem?.pathLink} className={`fw-bold ${router?.pathname === navItem?.pathLink ? "active" : ""}`}>
+            {currentLanguage === "TH" ? navItem.name?.TH : navItem.name?.EN}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
   return (
 
     <>
@@ -10,16 +28,11 @@ const TheNavber: React.FC = () => {
           <div className="row">
             <div className="col-12">
               <nav className="main-nav">
-                <a href="index" className="logo">
+                <a href="/" className="logo m-auto">
                   <img src="/images/logo.png" alt="" />
                 </a>
                 <ul className="nav">
-                  <li><a href="index" className="active">หน้าแรก</a></li>
-                  <li><a href="explore">ข่าว</a></li>
-                  <li><a href="details">เกี่ยวกับเรา</a></li>
-                  <li><a href="author">ผลงาน</a></li>
-                  <li><a href="create">ติดต่อเรา</a></li>
-
+                  {navList}
                 </ul>
                 <BtnSetting />
                 <a className='menu-trigger'>
