4ba7aec6bbb140b3d4cd3189f04757e517fb58e3
devilzeros <devilzeros@outlook.co.th>
2023-07-12 15:57:08 +0700

    est

--- components/TheHeadFirst.tsx
+++ components/TheHeadFirst.tsx
@@ -10,8 +10,8 @@ const TheHeadFirst: React.FC = () => {
                     <div className="col-lg-6 align-self-center">
                         <div className="header-text">
                             <h6>Liberty NFT Market</h6>
-                            <h2>Create, Sell &amp; Collect Top NFTs.</h2>
-                            <p>Liberty NFT Market is a really cool and professional design for your NFT websites. This HTML CSS template is based on Bootstrap v5 and it is designed for NFT related web portals. Liberty can be freely downloaded from TemplateMo's free css templates.</p>
+                            <h2>Create, Sell &amp; Collect Top NFT&apos;s.</h2>
+                            <p>Liberty NFT Market is a really cool and professional design for your NFT websites. This HTML CSS template is based on Bootstrap v5 and it is designed for NFT related web portals. Liberty can be freely downloaded from TemplateMo&apos;s free css templates.</p>
                             <div className="buttons">
                                 <div className="border-button">
                                     <a href="explore">Explore Top NFTs</a>
@@ -34,7 +34,7 @@ const TheHeadFirst: React.FC = () => {
                         <Carousel indicators={false}>
                             <Carousel.Item>
                                 {/* <div className="item"> */}
-                                <img src="/images/banner-01.png" className='p-3' alt="" />
+                                    <img src="/images/banner-01.png" className='p-3' alt="" />
                                 {/* </div> */}
                                 {/* <Carousel.Caption>
                                     <h3>Second slide label</h3>
@@ -43,7 +43,7 @@ const TheHeadFirst: React.FC = () => {
                             </Carousel.Item>
                             <Carousel.Item>
                                 {/* <div className="item"> */}
-                                <img src="/images/banner-02.png" className='p-3' alt="" />
+                                    <img src="/images/banner-02.png" className='p-3' alt="" />
                                 {/* </div> */}
                                 {/* <Carousel.Caption>
                                     <h3>Third slide label</h3>
