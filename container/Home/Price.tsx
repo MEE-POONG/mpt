aaf44b2eb15ae7e2db4771d3401507f671c29e98
devilzeros <devilzeros@outlook.co.th>
2023-07-15 19:54:01 +0700

    est

--- container/Home/Price.tsx
+++ container/Home/Price.tsx
@@ -1,6 +1,51 @@
-import React from 'react';
+
+import React, { useContext, useEffect, useState } from 'react';
+import { LanguageContext } from '@/components/Language/LanguageContext';
+
 import { priceHead, price } from '@/data/home';
+import Slider from 'react-slick';
+import { FaAngleLeft, FaAngleRight } from 'react-icons/fa';
+interface ArrowProps {
+    className?: string;
+    style?: React.CSSProperties;
+    onClick?: () => void;
+}
+const NextArrow: React.FC<ArrowProps> = (props) => {
+    const { className, style, onClick } = props;
+    return (
+        <div
+            className={`price-next-arrow ${className}`}
+            // style={{ ...style, display: 'flex', background: 'red' }}
+            onClick={onClick}
+        >
+            <FaAngleRight />
+        </div>
+    );
+}
+
+const PrevArrow: React.FC<ArrowProps> = (props) => {
+    const { className, style, onClick } = props;
+    return (
+        <div
+            className={`price-prev-arrow ${className}`}
+            // style={{ ...style, display: 'flex', background: 'green' }}
+            onClick={onClick}
+        >
+            <FaAngleLeft />
+        </div>
+    );
+}
 const Price: React.FC = () => {
+    const { currentLanguage } = useContext(LanguageContext);
+    const settings = {
+        dots: false,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        nextArrow: <NextArrow />,
+        prevArrow: <PrevArrow />
+    };
     return (
         <div className="create-nft">
             <div className="container">
@@ -8,50 +53,64 @@ const Price: React.FC = () => {
                     <div className="col-lg-8">
                         <div className="section-heading">
                             <div className="line-dec"></div>
-                            <h2>Create Your NFT & Put It On The Market.</h2>
+                            <h2>
+                                {currentLanguage === "TH" ? priceHead[0].title?.TH : priceHead[0].title?.EN}
+                            </h2>
+                            <h4>
+                                {currentLanguage === "TH" ? priceHead[0].detail?.TH : priceHead[0].detail?.EN}
+                            </h4>
                         </div>
                     </div>
                     <div className="col-lg-4">
                         <div className="main-button">
-                            <a href="create">Create Your NFT Now</a>
-                        </div>
-                    </div>
-                    <div className="col-lg-4">
-                        <div className="item first-item">
-                            <div className="number">
-                                <h6>1</h6>
-                            </div>
-                            <div className="icon">
-                                <img src="/images/icon-02.png" alt="" />
-                            </div>
-                            <h4>Set Up Your Wallet</h4>
-                            <p>NFT means Non-Fungible Token that are used in digital cryptocurrency markets. There are many different kinds of NFTs in the industry.</p>
-                        </div>
-                    </div>
-                    <div className="col-lg-4">
-                        <div className="item second-item">
-                            <div className="number">
-                                <h6>2</h6>
-                            </div>
-                            <div className="icon">
-                                <img src="/images/icon-04.png" alt="" />
-                            </div>
-                            <h4>Add Your Digital NFT</h4>
-                            <p>There are 5 different HTML pages included in this NFT <a href="https://templatemo.com/page/1" target="_blank" rel="nofollow">website template</a>. You can edit or modify any section on any page as you required.</p>
-                        </div>
-                    </div>
-                    <div className="col-lg-4">
-                        <div className="item">
-                            <div className="icon">
-                                <img src="/images/icon-06.png" alt="" />
-                            </div>
-                            <h4>Sell Your NFT &amp; Make Profit</h4>
-                            <p>If you would like to support our TemplateMo website, please visit <a rel="nofollow" href="https://templatemo.com/contact" target="_parent">our contact page</a> to make a PayPal contribution. Thank you.</p>
+                            <a href="/">{currentLanguage === "TH" ? "ซื้อแพ็คเกจ" : "Buy Package"}</a>
                         </div>
                     </div>
                 </div>
-            </div>
-        </div>
+                <Slider {...settings}>
+                    {price.map((item, index) => (
+
+                        <div className="item p-3 mt-0">
+
+                            <h4 className='d-flex align-items-center justify-content-center mt-0'>
+                                <div className="icon d-flex me-2">
+                                    <img src={`/images/icon-0${index + 2}.png`} alt="" className='m-auto' />
+                                </div>
+                                {item.title}
+                            </h4>
+                            <div className="row">
+                                <div className="col-lg-12">
+                                    {/* <p>{item.logo}</p>
+                                    <p>{item.webSelect}</p>
+                                    <p>{item.backOffice}</p>
+                                    <p>{item.codeUse}</p>
+                                    <p>{item.responsive}</p>
+                                    <p>{item.webDomain}</p>
+                                    <p>{item.ssl}</p>
+                                    <p>{item.page}</p>
+                                    <p>{item.pageList}</p>
+                                    <p>{item.storage}</p>
+                                    <p>{item.manual}</p>
+                                    <p>{item.Language}</p> */}
+                                    <div className="row">
+                                        <div className="col-6">
+                                            <span>
+                                                Current Bid: <br /> <strong>2.03 ETH</strong>
+                                            </span>
+                                        </div>
+                                        <div className="col-6">
+                                            <span>
+                                                Ends In: <br /> <strong>25th Nov</strong>
+                                            </span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                </Slider>
+            </div >
+        </div >
     );
 }
 
