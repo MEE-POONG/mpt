7b14eb1809b54262dbd8704648dd287358334727
wechanon <wechanon2001@gmail.com>
2023-07-18 10:55:43 +0700

    tiw

--- components/TheHeadFirst.tsx
+++ components/TheHeadFirst.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import { Carousel } from 'react-bootstrap';
+import { FaPencilRuler } from 'react-icons/fa';
+import { GiShop } from 'react-icons/gi';
+
 
 const TheHeadFirst: React.FC = () => {
     return (
@@ -7,17 +10,20 @@ const TheHeadFirst: React.FC = () => {
         <div className="main-banner">
             <div className="container">
                 <div className="row">
-                    <div className="col-lg-6 align-self-center">
+                    <div className="col-lg-6 align-self-center ">
                         <div className="header-text">
-                            <h6>Liberty NFT Market</h6>
-                            <h2>Create, Sell &amp; Collect Top NFT&apos;s.</h2>
-                            <p>Liberty NFT Market is a really cool and professional design for your NFT websites. This HTML CSS template is based on Bootstrap v5 and it is designed for NFT related web portals. Liberty can be freely downloaded from TemplateMo&apos;s free css templates.</p>
+                            <h6>รับทำเว็บไซต์</h6>
+                            <h2>ME PROMPT &amp; TECHNOLOGY&apos;</h2>
+                            <p>รับทำเว็บไซต์และการตลาดออนไลน์ครบวงจร ไม่ว่าธุรกิจของคุณ จะมีขนาดเล็กหรือใหญ่ 
+                                เราช่วยให้ธุรกิจของคุณเติบโตในโลกออนไลน์ ขยายขีดจำกัดการทำเว็บไซต์ พร้อมฟีเจอร์ธุรกิจออนไลน์ 
+                                ยกระดับการทำเว็บไซต์ของคุณ ด้วยเครื่องมือออกแบบเว็บไซต์สุดล้ำ ตอบสนองทุกไอเดียธุรกิจของคุณ 
+                                ระบบร้านค้าออนไลน์ที่ทันสมัย นำเสนอสินค้าน่าสนใจ และสะดวกในการซื้อขาย เพื่อเปิดโอกาสให้ธุรกิจคุณ</p>
                             <div className="buttons">
                                 <div className="border-button">
-                                    <a href="explore">Explore Top NFTs</a>
+                                    <a href="explore"><FaPencilRuler size={20}/>ออกแบบอย่างอิสระ</a>
                                 </div>
                                 <div className="main-button">
-                                    <a href="https://youtube.com/templatemo" target="_blank">Watch Our Videos</a>
+                                    <a href="https://youtube.com/templatemo" target="_blank"><GiShop size={20}/>ระบบร้านค้าเต็มรูปแบบ</a>
                                 </div>
                             </div>
                         </div>
--- container/CreateNft.tsx
+++ container/CreateNft.tsx
@@ -8,39 +8,43 @@ const CreateNft: React.FC = () => {
                     <div className="col-lg-8">
                         <div className="section-heading">
                             <div className="line-dec"></div>
-                            <h2>Create Your NFT & Put It On The Market.</h2>
+                            <h2>ME PROMPT &amp; TECHNOLOGY</h2>
                         </div>
                     </div>
                     <div className="col-lg-4">
-                        <div className="main-button">
+                        {/* <div className="main-button">
                             <a href="create">Create Your NFT Now</a>
-                        </div>
+                        </div> */}
                     </div>
                     <div className="col-lg-4">
                         <div className="item first-item">
                             <div className="number">
-                                <h6>1</h6>
+                                {/* <h6>1</h6> */}
                             </div>
                             <div className="icon">
                                 <img src="/images/icon-02.png" alt="" />
                             </div>
-                            <h4>Set Up Your Wallet</h4>
-                            <p>NFT means Non-Fungible Token that are used in digital cryptocurrency markets. There are many different kinds of NFTs in the industry.</p>
+                            <h4>เกี่ยวกับเรา</h4>
+                            <p>รับทำเว็บไซต์และการตลาดออนไลน์ครบวงจร ไม่ว่าธุรกิจของคุณ จะมีขนาดเล็กหรือใหญ่ 
+                                เราช่วยให้ธุรกิจของคุณเติบโตในโลกออนไลน์ ขยายขีดจำกัดการทำเว็บไซต์ พร้อมฟีเจอร์ธุรกิจออนไลน์</p>
                         </div>
                     </div>
                     <div className="col-lg-4">
                         <div className="item second-item">
                             <div className="number">
-                                <h6>2</h6>
+                                {/* <h6>2</h6> */}
                             </div>
                             <div className="icon">
                                 <img src="/images/icon-04.png" alt="" />
                             </div>
-                            <h4>Add Your Digital NFT</h4>
-                            <p>There are 5 different HTML pages included in this NFT <a href="https://templatemo.com/page/1" target="_blank" rel="nofollow">website template</a>. You can edit or modify any section on any page as you required.</p>
+                            <h4>ติดต่อเรา</h4>
+                            <p>46/3 Ratchanikun Rd, Mueang Nakhon Ratchasima District, Nakhon Ratchasima 30000
+                            meeprompt.teach@gmail.com
+                            มือถือ 091 813 6426
+</p>
                         </div>
                     </div>
-                    <div className="col-lg-4">
+                    {/* <div className="col-lg-4">
                         <div className="item">
                             <div className="icon">
                                 <img src="/images/icon-06.png" alt="" />
@@ -48,7 +52,7 @@ const CreateNft: React.FC = () => {
                             <h4>Sell Your NFT &amp; Make Profit</h4>
                             <p>If you would like to support our TemplateMo website, please visit <a rel="nofollow" href="https://templatemo.com/contact" target="_parent">our contact page</a> to make a PayPal contribution. Thank you.</p>
                         </div>
-                    </div>
+                    </div> */}
                 </div>
             </div>
         </div>
