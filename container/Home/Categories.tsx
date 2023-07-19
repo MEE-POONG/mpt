7b441d19cdf765a4c07042f685961403dd5868a8
devilzeros <devilzeros@outlook.co.th>
2023-07-19 11:31:50 +0700

    est

--- container/Home/Categories.tsx
+++ container/Home/Categories.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { FaAngleLeft, FaAngleRight } from 'react-icons/fa';
 import Slider from 'react-slick';
 import { SiTypescript } from "react-icons/si";
+import { Container } from 'react-bootstrap';
 
 interface ArrowProps {
     className?: string;
@@ -74,35 +75,23 @@ const Categories: React.FC = () => {
         dots: false,
         infinite: true,
         speed: 500,
-        slidesToShow: 3,
+        slidesToShow: 4,
         slidesToScroll: 1,
         nextArrow: <NextArrow />,
         prevArrow: <PrevArrow />,
         responsive: [
             {
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 4,
-                }
-            },
-            {
-                breakpoint: 700,
-                settings: {
-                    slidesToShow: 3,
-                }
-            },
-            {
-                breakpoint: 500,
+                breakpoint: 1199,
                 settings: {
                     slidesToShow: 2,
                 }
             },
             {
-                breakpoint: 320,
+                breakpoint: 676,
                 settings: {
                     slidesToShow: 1,
                 }
-            }
+            },
         ]
     };
     return (
@@ -122,7 +111,7 @@ const Categories: React.FC = () => {
                                     <div className="icon">
                                         <img className="d-inline" src="https://imagedelivery.net/QZ6TuL-3r02W7wQjQrv5DA/89a103ef-d0af-4fad-9970-a3c278f9ec00/170" alt="" />
                                     </div>
-                                    <h4>Html5</h4>
+                                    <h4 className='truncate'>Html5</h4>
                                     <div className="icon-button">
                                         <a href="https://www.designil.com/html5-tips-tricks-techniques-1/"><FaAngleRight /></a>
                                     </div>
@@ -133,7 +122,7 @@ const Categories: React.FC = () => {
                                     <div className="icon">
                                         <img className="d-inline" src="https://imagedelivery.net/QZ6TuL-3r02W7wQjQrv5DA/e311df22-7550-4e6a-3dd7-367e504b2900/170" alt="" />
                                     </div>
-                                    <h4>Css</h4>
+                                    <h4 className='truncate'>Css</h4>
                                     <div className="icon-button">
                                         <a href="http://www.kontentblue.com/site/article/article.php?id=css-introduction"><FaAngleRight /></a>
                                     </div>
@@ -144,7 +133,7 @@ const Categories: React.FC = () => {
                                     <div className="icon">
                                         <img className="d-inline" src="https://imagedelivery.net/QZ6TuL-3r02W7wQjQrv5DA/abf7786a-c158-4a38-aab6-dec67822af00/170" alt="" />
                                     </div>
-                                    <h4>Es6</h4>
+                                    <h4 className='truncate'>Es6</h4>
                                     <div className="icon-button">
                                         <a href="https://microbenz.in.th/%E0%B8%99%E0%B8%B5%E0%B9%88%E0%B8%9B%E0%B8%B5-2017-%E0%B9%81%E0%B8%A5%E0%B9%89%E0%B8%A7-%E0%B8%A1%E0%B8%B2%E0%B9%80%E0%B8%82%E0%B8%B5%E0%B8%A2%E0%B8%99-es6-%E0%B8%81%E0%B8%B1%E0%B8%99-9dede81e30da"><FaAngleRight /></a>
                                     </div>
@@ -155,7 +144,7 @@ const Categories: React.FC = () => {
                                     <div className="icon">
                                         <img className="d-inline" src="https://imagedelivery.net/QZ6TuL-3r02W7wQjQrv5DA/557b6253-75ac-4c99-4e8c-3cecee80db00/170" alt="" />
                                     </div>
-                                    <h4>Flutter</h4>
+                                    <h4 className='truncate'>Flutter</h4>
                                     <div className="icon-button">
                                         <a href="https://www.thaiprogrammer.org/2019/11/flutter-framework/"><FaAngleRight /></a>
                                     </div>
@@ -166,7 +155,7 @@ const Categories: React.FC = () => {
                                     <div className="icon">
                                         <img className="d-inline" src="https://imagedelivery.net/QZ6TuL-3r02W7wQjQrv5DA/52a4fe9d-abf5-4e53-9d10-e0d5877cbc00/170" alt="" />
                                     </div>
-                                    <h4>Sass</h4>
+                                    <h4 className='truncate'>Sass</h4>
                                     <div className="icon-button">
                                         <a href="https://www.borntodev.com/2020/07/15/sass-scss-101/"><FaAngleRight /></a>
                                     </div>
@@ -177,7 +166,7 @@ const Categories: React.FC = () => {
                                     <div className="icon">
                                         <img className="d-inline" src="/images/icon-06.png" alt="" />
                                     </div>
-                                    <h4>Framework</h4>
+                                    <h4 className='truncate'>Framework</h4>
                                     <div className="icon-button">
                                         <a href="https://devahoy.com/getting-started-with-nextjs/"><FaAngleRight /></a>
                                     </div>
@@ -192,12 +181,12 @@ const Categories: React.FC = () => {
                     <div className="line-dec"></div>
                     <h2>Explore Some Hot <em>Portfolio</em> By MePrompt.</h2>
                 </div>
-                <div className='meprompt-portfolio container'>
+                <div className='meprompt-portfolio'>
                     <Slider {...settings}>
                         <div className="item px-2">
-                            <img src="/images/customers/changpong.png" width="500" height="250" alt="" />
+                            <img src="/images/customers/changpong.png" alt="" />
                             <div className="down-content">
-                                <h4>ช่างพองอิฐประสาน</h4>
+                                <h4 className='truncate'>ช่างพองอิฐประสาน</h4>
                                 <span className="collection">Viewer :<br /><strong>310</strong></span>
                                 <span className="category">Category:<br /><strong>เว็บบริษัท/องค์กร</strong></span>
                                 <div className="main-button">
@@ -206,9 +195,9 @@ const Categories: React.FC = () => {
                             </div>
                         </div>
                         <div className="item px-2">
-                            <img src="/images/customers/homeagain.png" width="500" height="250" alt="" />
+                            <img src="/images/customers/homeagain.png" alt="" />
                             <div className="down-content">
-                                <h4>มูลนิธิบ้านหลังใหม่</h4>
+                                <h4 className='truncate'>มูลนิธิบ้านหลังใหม่</h4>
                                 <span className="collection">Viewer :<br /><strong>324</strong></span>
                                 <span className="category">Category:<br /><strong>เว็บบริษัท/องค์กร</strong></span>
                                 <div className="main-button">
@@ -217,9 +206,9 @@ const Categories: React.FC = () => {
                             </div>
                         </div>
                         <div className="item px-2">
-                            <img src="/images/customers/robot.png" width="500" height="250" alt="" />
+                            <img src="/images/customers/robot.png" alt="" />
                             <div className="down-content">
-                                <h4>Robot รถตัดหญ้าบังคับวิทยุ</h4>
+                                <h4 className='truncate'>Robot รถตัดหญ้าบังคับวิทยุ</h4>
                                 <span className="collection">Viewer :<br /><strong>380</strong></span>
                                 <span className="category">Category:<br /><strong>เว็บบริษัท/องค์กร</strong></span>
                                 <div className="main-button">
@@ -228,9 +217,9 @@ const Categories: React.FC = () => {
                             </div>
                         </div>
                         <div className="item px-2">
-                            <img src="/images/customers/fairy.png" width="500" height="250" alt="" />
+                            <img src="/images/customers/fairy.png" alt="" />
                             <div className="down-content">
-                                <h4>Fairy Stone - หินนำโชค</h4>
+                                <h4 className='truncate'>Fairy Stone - หินนำโชค</h4>
                                 <span className="collection">Viewer :<br /><strong>426</strong></span>
                                 <span className="category">Category:<br /><strong>เว็บบริษัท/องค์กร</strong></span>
                                 <div className="main-button">
@@ -239,9 +228,9 @@ const Categories: React.FC = () => {
                             </div>
                         </div>
                         <div className="item px-2">
-                            <img src="/images/customers/fairy.png" width="500" height="250" alt="" />
+                            <img src="/images/customers/fairy.png" alt="" />
                             <div className="down-content">
-                                <h4>Fairy Stone - หินนำโชค</h4>
+                                <h4 className='truncate'>Fairy Stone - หินนำโชค</h4>
                                 <span className="collection">Viewer :<br /><strong>426</strong></span>
                                 <span className="category">Category:<br /><strong>เว็บบริษัท/องค์กร</strong></span>
                                 <div className="main-button">
@@ -250,9 +239,9 @@ const Categories: React.FC = () => {
                             </div>
                         </div>
                         <div className="item px-2">
-                            <img src="/images/customers/fairy.png" width="500" height="250" alt="" />
+                            <img src="/images/customers/fairy.png" alt="" />
                             <div className="down-content">
-                                <h4>Fairy Stone - หินนำโชค</h4>
+                                <h4 className='truncate'>Fairy Stone - หินนำโชค</h4>
                                 <span className="collection">Viewer :<br /><strong>426</strong></span>
                                 <span className="category">Category:<br /><strong>เว็บบริษัท/องค์กร</strong></span>
                                 <div className="main-button">
