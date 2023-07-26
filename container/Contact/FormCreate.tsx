a6a3d0d8f37c624ea5158098cc9b0def9681270e
devilzeros <devilzeros@outlook.co.th>
2023-07-26 17:44:55 +0700

    est

--- container/Contact/FormCreate.tsx
+++ container/Contact/FormCreate.tsx
@@ -53,50 +53,11 @@ const FormCreateCopy: React.FC = () => {
                             </Form.Group>
                         </div>
                         <div className="col-lg-8">
-                            <button type="submit" id="form-submit" className="orange-button">Submit Your Applying</button>
+                            <button type="submit" id="form-submit" className="purple">Submit Your Applying</button>
                         </div>
 
                     </Row>
                 </form>
-                {/* <div className="col-lg-12">
-                        <div className="section-heading">
-                            <div className="line-dec"></div>
-                            <h2>This Is <em>Your Item</em> Preview.</h2>
-                        </div>
-                    </div> */}
-                {/* <div className="col-lg-7">
-                        <div className="left-image">
-                            <img src="assets/images/create-yours.jpg" alt="" style="border-radius: 20px;">
-                        </div>
-                    </div> */}
-                {/* <div className="col-lg-5 align-self-center">
-                        <h4>Dolores Haze Westworld Eye</h4>
-                        <span className="author">
-                            <img src="assets/images/author-02.jpg" alt="" style="max-width: 50px; border-radius: 50%;">
-                                <h6>Kataleya Smithee<br><a href="#">@kataleey</a></h6>
-                        </span>
-                        <p>Lorem ipsum dolor sit amet, consectetu dipiscingei elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                        <div className="row">
-                            <div className="col-3">
-                                <span className="bid">
-                                    Current Bid<br><strong>0.06 ETH</strong><br><em>($8055,35)</em>
-                                    </span>
-                                    </div>
-                                    <div className="col-4">
-                                        <span className="owner">
-                                            Owner<br><strong>Alan Smithee</strong><br><em>(@asmithee)</em>
-                                            </span>
-                                            </div>
-                                            <div className="col-5">
-                                                <span className="ends">
-                                                    Ends In<br><strong>3D 05H 20M 58S</strong><br><em>(January 22nd, 2021)</em>
-                                                    </span>
-                                                    </div>
-                                            </div>
-                                    </div>
-                            </div>
-                        </div>
-                    </div> */}
             </div>
         </div >
     );
